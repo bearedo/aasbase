01f01818b50fad46e3848ca9bf448481e34e422c
doug.beare <doug.beare@8d699c2c-0053-b93d-9449-7fcc838bc665>
2013-07-17 12:08:59 +0000

    Just builds 1970 onwards

--- trunk/db_input_scripts/BuildTheGlobal-ICOADSDB.pl
+++ trunk/db_input_scripts/BuildTheGlobal-ICOADSDB.pl
@@ -25,15 +25,16 @@ require "aas_db.pl";
 
 
 print PSQL ("SET SEARCH_PATH TO global; \n");
-##print PSQL ("DROP TABLE global.clim_icoads_world CASCADE; \n");
-#
+print PSQL ("DROP TABLE global.clim_icoads_world_new CASCADE; \n");
+
 #
 ##print PSQL ("CREATE TABLESPACE dbspace LOCATION \'G://pGdata\'; \n");
 #
 #print PSQL ("SET default_tablespace = dbspace; \n");
 #
-print PSQL ("CREATE TABLE global.clim_icoads_world (
+print PSQL ("CREATE TABLE global.clim_icoads_world_new (
 yr int, mo int,dy int,hr float,lat float4,lon float4,im int,ti int,li int,ii int,
+
 id varchar(12),c1 varchar(3),
 di int,
 d int,   
@@ -54,89 +55,90 @@ sh int) WITH OIDS;\n");
 
 #print "$yr|$mo|$dy|$hr|$lat|$lon|$im|$ti|$li|$ii|$id|$c1|$di|$d|$wi|$w|$vv|$ww|$slp|$at|$sst|$n|$wd|$wp|$wh|$sd|$sp|$sh\n";
 
-print PSQL ("COMMENT ON TABLE global.icoads IS 'These are surface data (1784-2012) from icoads in most up to date format (www.cdc.noaa.gov). Future data can be downloaded via ftp with the help of Zaihua';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.yr IS 'Year (-9999)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.mo IS 'Month (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.dy IS 'Day (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.hr IS 'Hour 0.01hr (-99.99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.lat IS 'Digital latitude 0.01N (-999.99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.lon IS 'Digital longitude 0.01E (-999.99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.ti IS 'Time indic.              (-9)';\n");
-#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.ds IS 'Ship course               (-9)';\n");
-#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.vs IS 'Ship speed  knots         (-9)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.ii IS 'ID indicator              (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.id IS 'ID ident/call sign        (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.c1 IS 'Country code              (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.di IS 'Wind  Direc. indic.          (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.d IS 'Wind  Direction    degree    (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.wi IS 'Wind  Speed Indicator    degree    (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.w IS 'Wind  Speed   0.1m/s     (-9)';\n");
-#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.vi IS 'vv indic           (-9)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.vv IS 'Visbility           (-9)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.ww IS 'Present weather           (-9)';\n");
-#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.w1 IS 'Past weather           (-9)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.slp IS 'Sea level pressure  0.1hPa   (-9999.9)';\n");
-#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.ppp IS 'Atmospheric pressure tendency   (-99.9)';\n");
-##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.it IS 'Temperature indicator   (-9)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.at IS 'Air temperature 0.1C   (-999.9)';\n");
-##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.wbti IS 'Indicator for wbt 0.1C   (-999.9)';\n");
-##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.wbt IS ' Wet bulb temp. 0.1C     (-999.9)';\n");
-##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.dpti IS 'Dpt indicator              (-9)';\n");
-##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.dpt IS 'Dew point temperature  0.1C  (-999.9)';\n");
-##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.si IS 'SST measurement method         (-99)';\n");
-#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.sst IS 'Sea surface temperature  0.1C  (-999.9)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.n IS 'Total cloud amt                  (-9)';\n");
-##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.cl IS 'Low cloud type                   (-999.9)';\n");
-##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.hi IS 'H indic.                             (-9)';\n");
-##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.h IS 'Cloud height    (-9)';\n");
-##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.cm IS 'Mid-cloud height    (-9)';\n");
-##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.ch IS 'High-cloud height    (-9)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.wd IS 'Wave direction       (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.wp IS 'Wave period       (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.wh IS 'Wave height       (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.sd IS 'Swell direction    (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.sp IS 'Swell period       (-99)';\n");
-print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.sh IS 'Swell height       (-99)';\n");
-#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.statsq IS 'ICES Statistical Rectangle';\n");
-#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.dck IS 'Deck              (-999)';\n");
-#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.sid IS 'Source ID          (-999)';\n");
-#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world.pt  IS 'Platform type          (-999)';\n");
+print PSQL ("COMMENT ON TABLE global.clim_icoads_world_new IS 'These are surface data (1970-present) from icoads in most up to date format (www.cdc.noaa.gov). Future data can be downloaded via ftp with the help of Zaihua';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.yr IS 'Year (-9999)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.mo IS 'Month (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.dy IS 'Day (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.hr IS 'Hour 0.01hr (-99.99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.lat IS 'Digital latitude 0.01N (-999.99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.lon IS 'Digital longitude 0.01E (-999.99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.ti IS 'Time indic.              (-9)';\n");
+#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.ds IS 'Ship course               (-9)';\n");
+#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.vs IS 'Ship speed  knots         (-9)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.ii IS 'ID indicator              (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.id IS 'ID ident/call sign        (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.c1 IS 'Country code              (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.di IS 'Wind  Direc. indic.          (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.d IS 'Wind  Direction    degree    (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.wi IS 'Wind  Speed Indicator    degree    (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.w IS 'Wind  Speed   0.1m/s     (-9)';\n");
+#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.vi IS 'vv indic           (-9)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.vv IS 'Visbility           (-9)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.ww IS 'Present weather           (-9)';\n");
+#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.w1 IS 'Past weather           (-9)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.slp IS 'Sea level pressure  0.1hPa   (-9999.9)';\n");
+#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.ppp IS 'Atmospheric pressure tendency   (-99.9)';\n");
+##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.it IS 'Temperature indicator   (-9)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.at IS 'Air temperature 0.1C   (-999.9)';\n");
+##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.wbti IS 'Indicator for wbt 0.1C   (-999.9)';\n");
+##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.wbt IS ' Wet bulb temp. 0.1C     (-999.9)';\n");
+##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.dpti IS 'Dpt indicator              (-9)';\n");
+##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.dpt IS 'Dew point temperature  0.1C  (-999.9)';\n");
+##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.si IS 'SST measurement method         (-99)';\n");
+#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.sst IS 'Sea surface temperature  0.1C  (-999.9)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.n IS 'Total cloud amt                  (-9)';\n");
+##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.cl IS 'Low cloud type                   (-999.9)';\n");
+##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.hi IS 'H indic.                             (-9)';\n");
+##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.h IS 'Cloud height    (-9)';\n");
+##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.cm IS 'Mid-cloud height    (-9)';\n");
+##print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.ch IS 'High-cloud height    (-9)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.wd IS 'Wave direction       (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.wp IS 'Wave period       (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.wh IS 'Wave height       (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.sd IS 'Swell direction    (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.sp IS 'Swell period       (-99)';\n");
+print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.sh IS 'Swell height       (-99)';\n");
+#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.statsq IS 'ICES Statistical Rectangle';\n");
+#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.dck IS 'Deck              (-999)';\n");
+#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.sid IS 'Source ID          (-999)';\n");
+#print PSQL ("COMMENT ON COLUMN global.clim_icoads_world_new.pt  IS 'Platform type          (-999)';\n");
 #
 # FILL THE TABLE
 # Global East
-open(DATAFILE, "ls /srv/public/input_data_files/ICOADS-Global/ICOADS-East/tmp/*dat |");
+open(DATAFILE, "ls /srv/public/input_data_files/ICOADS-Global/ICOADS-East/tmp1970/*dat |");
 while(<DATAFILE>){
 s/^\s*(.*)\s*$/$1/;
 chomp;
 open(FILE,"<$_") or die "Cannot open file $_: $!\n";
-print PSQL ("\\COPY global.clim_icoads_world FROM \'$_\' with delimiter \'|\' null as \'NA\'\n");
+print PSQL ("\\COPY global.clim_icoads_world_new FROM \'$_\' with delimiter \'|\' null as \'NA\'\n");
 print "IMPORTING: $_\n";}
 
 # Global West
-open(DATAFILE, "ls /srv/public/input_data_files/ICOADS-Global/ICOADS-West/tmp/*dat |");
+open(DATAFILE, "ls /srv/public/input_data_files/ICOADS-Global/ICOADS-West/tmp1970/*dat |");
 while(<DATAFILE>){
 s/^\s*(.*)\s*$/$1/;
 chomp;
 open(FILE,"<$_") or die "Cannot open file $_: $!\n";
-print PSQL ("\\COPY global.clim_icoads_world FROM \'$_\' with delimiter \'|\' null as \'NA\'\n");
+print PSQL ("\\COPY global.clim_icoads_world_new FROM \'$_\' with delimiter \'|\' null as \'NA\'\n");
 print "IMPORTING: $_\n";}
 
 close(DATAFILE);
 
+
 ## Drop some columns to save space and speed queries ##
 
-#print PSQL ("
-#ALTER TABLE global.clim_icoads_world DROP COLUMN im CASCADE;
-#ALTER TABLE global.clim_icoads_world DROP COLUMN ii CASCADE;
-#ALTER TABLE global.clim_icoads_world DROP COLUMN id CASCADE;
-#ALTER TABLE global.clim_icoads_world DROP COLUMN vv CASCADE;
-#ALTER TABLE global.clim_icoads_world DROP COLUMN ww CASCADE;
-#\n");
+print PSQL ("
+ALTER TABLE global.clim_icoads_world_new DROP COLUMN im CASCADE;
+ALTER TABLE global.clim_icoads_world_new DROP COLUMN ii CASCADE;
+ALTER TABLE global.clim_icoads_world_new DROP COLUMN id CASCADE;
+ALTER TABLE global.clim_icoads_world_new DROP COLUMN vv CASCADE;
+ALTER TABLE global.clim_icoads_world_new DROP COLUMN ww CASCADE;
+\n");
 
 # Drop data before 1900 to save space and speed queries 
 
 #print PSQL ("
-#DELETE FROM global.clim_icoads_world
+#DELETE FROM global.clim_icoads_world_new
 #  WHERE yr < 1900;
 #\n");
 
@@ -144,18 +146,21 @@ close(DATAFILE);
 #
 ## Create indices ###
 
-print PSQL ("CREATE INDEX icoads_world_mo ON global.clim_icoads_world (mo);\n");
-print PSQL ("CREATE INDEX icoads_world_yr ON global.clim_icoads_world (yr);\n");
-print PSQL ("CREATE INDEX icoads_world_lat ON global.clim_icoads_world (lat);\n");
-print PSQL ("CREATE INDEX icoads_world_lon ON global.clim_icoads_world (lon);\n");
+print PSQL ("CREATE INDEX icoads_world_mo ON global.clim_icoads_world_new (mo);\n");
+print PSQL ("CREATE INDEX icoads_world_yr ON global.clim_icoads_world_new (yr);\n");
+print PSQL ("CREATE INDEX icoads_world_lat ON global.clim_icoads_world_new (lat);\n");
+print PSQL ("CREATE INDEX icoads_world_lon ON global.clim_icoads_world_new (lon);\n");
+
+## Make all westerly longitudes icoads_world
+
+#print PSQL ("UPDATE global.clim_icoads_world_new SET lon = lon -360 WHERE lon > 180;\n");
 
 ### Add on geometry point ###
 
-print PSQL ("SET SEARCH_PATH to public;\n");
-print PSQL ("SELECT addgeometrycolumn('','global','clim_icoads_world','the_point',4326,'POINT',2);\n");
-print PSQL ("UPDATE global.clim_icoads_world SET the_point=SETSRID(MAKEPOINT(lon,lat),4326);\n"); 
-print PSQL ("CREATE INDEX icoads_world_the_point ON global.clim_icoads_world USING GIST (the_point);\n");
+print PSQL ("ALTER TABLE global.clim_icoads_world_new ADD COLUMN the_point geometry(Point,4326);\n");
+print PSQL ("UPDATE global.clim_icoads_world_new SET the_point = ST_SETSRID(ST_MAKEPOINT(lon,lat),4326);\n"); 
 
+print PSQL ("CREATE INDEX icoads_world_the_point ON global.clim_icoads_world_new USING GIST (the_point);\n");
 
 
 #print "Converting lat/lon position into ICES stat square\n";
