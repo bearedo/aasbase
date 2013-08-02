19eefd74b4af9e9db934b86966946b7f6c764f56
doug.beare <doug.beare@8d699c2c-0053-b93d-9449-7fcc838bc665>
2013-08-02 02:21:59 +0000

    New file to import Dutch fisheries data

---
+++ trunk/db_input_scripts/importDutchTacsatEflalo.sh
@@ -0,0 +1,19 @@
+TMPDIR="/srv/public/input_data_files/"
+DIR="/srv/public/input_data_files/NetherlandsLandingsVMSdata"
+SCHEMA="netherlands"
+DB="aas_base"
+USER_NAME="postgres"
+cd $DIR
+
+## Script written by Doug Beare to import sql files for tacsat (VMS) and landings (eflalo format) for the Netherlands 1990-2011. Data originally obtained from IMARES by Doug Beare.
+
+## Import the data 
+psql -d ${DB} -U postgres -c "DROP TABLE IF EXISTS eflalo;"
+psql -d ${DB} -U postgres -c "DROP TABLE IF EXISTS tacsat;"
+
+psql -d ${DB} -f eflalo.sql -U postgres
+psql -d ${DB} -f tacsat.sql -U postgres
+
+## Change schema
+
+psql -U postgres -c "ALTER TABLE eflalo SET SCHEMA netherlands; ALTER TABLE tacsat SET SCHEMA netherlands;"
